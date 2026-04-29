3fce145df43d5510b90e242190985bdc4ebbf7c4
Putriangelli75 <putriangelli75@gmail.com>
2026-04-29 14:32:40 +0700

    update

--- putriangelli-app/resources/views/produk/index.blade.php
+++ putriangelli-app/resources/views/produk/index.blade.php
@@ -1,3 +1,40 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+@extends('app.master')
+@section('title', $title)
+@section('sidebar')
+    @parent
+    @section('submenu-produk')
+        <a href="/produk/create" class="list-group-item list-group-item-action ps-4 
+        {{ request()->is('produk/create') ? 'active' : '' }}">Tambah Produk</a>
+        <a href="/produk/search" class="list-group-item list-group-item-action ps-4 
+        {{ request()->is('produk/search') ? 'active' : '' }}">Cari Produk</a>
+    @endsection
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <h1 class="mb-4">(($title))</h1>
+
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th>No</th>
+                    <th>Nama Produk</th>
+                    <th>Harga</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @for ($i = 0; $i < count($products); $i++)
+                    <tr>
+                        <td>(( $i + 1 ))</td>
+                        <td>(( $products[$i]['name'] ))</td>
+                        <td>Rp {( number_format[$i]['price'], 0, ',', '.') })</td>
+                        <td>
+                            <a href="{{ url('/produk/' . $products[$i]['id']) }}" classs="btn btn-sm btn-info"
+    <div class="card">
+        <div class="card-body">
+            Konten produk bisa ditampilkan di sini.
+        </div>
+    </div>
+@endsection
