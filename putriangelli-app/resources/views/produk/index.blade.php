978be97ab64d9e7af9c3aaf18b5f2d421324dc2e
Putriangelli75 <putriangelli75@gmail.com>
2026-04-30 21:40:14 +0700

    update

--- putriangelli-app/resources/views/produk/index.blade.php
+++ putriangelli-app/resources/views/produk/index.blade.php
@@ -2,17 +2,21 @@
 @section('title', $title)
 @section('sidebar')
     @parent
-    @section('submenu-produk')
-        <a href="/produk/create" class="list-group-item list-group-item-action ps-4 
-        {{ request()->is('produk/create') ? 'active' : '' }}">Tambah Produk</a>
-        <a href="/produk/search" class="list-group-item list-group-item-action ps-4 
-        {{ request()->is('produk/search') ? 'active' : '' }}">Cari Produk</a>
-    @endsection
+@section('submenu-produk')
+    <a href="/produk/create"
+        class="list-group-item list-group-item-action ps-4 
+        {{ request()->is('produk/create') ? 'active' : '' }}">Tambah
+        Produk</a>
+    <a href="/produk/search"
+        class="list-group-item list-group-item-action ps-4 
+        {{ request()->is('produk/search') ? 'active' : '' }}">Cari
+        Produk</a>
+@endsection
 @endsection
 
 @section('content')
 <div class="container-fluid">
-    <h1 class="mb-4">(($title))</h1>
+    <h1 class="mb-4">{{ $title }}</h1>
 
     <div class="table-responsive">
         <table class="table table-striped table-bordered table-hover">
@@ -25,16 +29,32 @@
                 </tr>
             </thead>
             <tbody>
-                @for ($i = 0; $i < count($products); $i++)
+                {{-- @for ($i = 0; $i < count($products); $i++)
                     <tr>
-                        <td>(( $i + 1 ))</td>
-                        <td>(( $products[$i]['name'] ))</td>
-                        <td>Rp {( number_format[$i]['price'], 0, ',', '.') })</td>
+                        <td>{{ $i + 1 }}</td>
+                        <td>{{ $products[$i]['name'] }}</td>
+                        <td>Rp {{ number_format($products[$i]['price'], 2, ',', '.') }}</td>
                         <td>
-                            <a href="{{ url('/produk/' . $products[$i]['id']) }}" classs="btn btn-sm btn-info"
-    <div class="card">
-        <div class="card-body">
-            Konten produk bisa ditampilkan di sini.
-        </div>
+                            <a href="{{ url('/produk/' . $products[$i]['id']) }}" class="btn btn-sm btn-info">Detail</a>
+                            <a href="{{ url('/produk/' . $products[$i]['id'] . '/edit') }}"
+                                class="btn btn-sm btn-primary">Edit</a>
+                        </td>
+                    </tr>
+                @endfor --}}
+                @foreach ($products as $item)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item['name'] }}</td>
+                        <td>Rp {{ number_format($item['price'], 2, ',', '.') }}</td>
+                        <td>
+                            <a href="{{ url('/produk/' . $item['id']) }}" class="btn btn-sm btn-info">Detail</a>
+                            <a href="{{ url('/produk/' . $item['id'] . '/edit') }}"
+                                class="btn btn-sm btn-primary">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+</div>
 @endsection
