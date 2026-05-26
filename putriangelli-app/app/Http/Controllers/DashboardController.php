3e6d9a4a72c55deec876596619a36a88421286a2
Putriangelli75 <putriangelli75@gmail.com>
2026-05-26 12:06:25 +0700

    update

--- putriangelli-app/app/Http/Controllers/DashboardController.php
+++ putriangelli-app/app/Http/Controllers/DashboardController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $totalBarang = Product::count();
+        $barangTersedia = Product::where('is_active', 1)->count();
+        $barangHabis = Product::where('is_active', 0)->count();
+        $nilaiStok = 'Rp ' . number_format(Product::sum('price'), 0, ',', '.');
+        $barangTerbaru = Product::latest()->take(5)->get();
+        return view('dashboard', compact(
+            'totalBarang',
+            'barangTersedia',
+            'barangHabis',
+            'nilaiStok',
+            'barangTerbaru'
+        ));
+    }
 }
--- putriangelli-app/app/Providers/AuthServiceProvider.php
+++ putriangelli-app/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -19,6 +21,21 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Untuk megelola product hanya dilakukan oleh admin
+        Gate::define('manage-products', function ($user) {
+            return $user->role === 'admin';
+        });
+        //Untuk update product dapat dilakukan oleh admin dan sales
+        Gate::define('update-product', function (User $user) {
+            return $user->role === 'admin' || $user->role === 'sales';
+        });
+        //Untuk menghapus product hanya dilakukan oleh admin
+        Gate::define('delete-product', function (User $user) {
+            return $user->role === 'admin';
+        });
+        //Untuk membuat product dapat dilakukan oleh user yang sudah login
+        Gate::define('create-product', function (User $user) {
+            return $user->role === 'sales';
+        });
     }
 }
